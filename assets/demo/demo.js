6f12370dc5ae5da8783c644574ca72f30e9badc2
Sazzad Sazib <sazib66@gmail.com>
2018-03-12 15:19:54 +0600

    updated

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -446,11 +446,11 @@ demo = {
     },
 
 	showNotification: function(from, align){
-    	color = 'primary';
+    	color = 'success';
 
     	$.notify({
-        	icon: "now-ui-icons ui-1_bell-53",
-        	message: "Welcome to <b>Now Ui Dashboard Pro</b> - a beautiful freebie for every web developer."
+        	icon: "now-ui-icons business_money-coins",
+        	message: "Payment Sent Successfully"
 
         },{
             type: color,
